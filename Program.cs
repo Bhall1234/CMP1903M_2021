778e878eee18fffcb5cab2b79c6182a1b1cc6324
Mark Doughty <mdoughty@lincoln.ac.uk>
2021-01-28 15:08:42 +0000

    blank C# project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Mark Doughty, 2021
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CMP1903M!");
+        }
+    }
+}
